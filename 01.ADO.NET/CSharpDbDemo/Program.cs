2b4362c5e3a84930b8114e1d26dba68b843cd6ce
vebili <74007431+vebili@users.noreply.github.com>
2021-02-17 10:38:08 +0200

    Add project files.

---
+++ 01.ADO.NET/CSharpDbDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using MyClasses;
+using System;
+
+namespace CSharpDbDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection("");
+            connection.Open();
+            var query = new SqlCommand("SELECT COUNT(*) FROM Employees");
+        }
+    }
+}
---
+++ 01.ADO.NET/MyClasses/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MyClasses
+{
+    public class Class1
+    {
+
+    }
+}
