05396f9bcc9921dec6ed36f7ce294be66b1448e0
vebili <74007431+vebili@users.noreply.github.com>
2021-02-26 14:17:05 +0200

    Add project files.

---
+++ 02.ORM-Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniORM.App
+{
+    using System.Linq;
+    using Data;
+    using Data.Entities;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.;Database=MiniORM;Integrated Security=True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Pesho",
+                LastName = "Peshev",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
