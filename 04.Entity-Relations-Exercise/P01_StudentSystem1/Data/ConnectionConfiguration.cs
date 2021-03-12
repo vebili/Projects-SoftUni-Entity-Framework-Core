5fb202ad47e5d116a4c2d1fd925bc9f28c4d3ed3
vebili <74007431+vebili@users.noreply.github.com>
2021-03-12 18:21:41 +0200

    Add project files.

--- 04.Entity-Relations-Exercise/P01_StudentSystem1/Data/ConnectionConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace P01_StudentSystem.Data
-{
-   internal static class ConnectionConfiguration
-    {
-        internal static string ConnectionString = @"Server=.;Database=StudentSystem;Integrated Security= true;";
-    }
-}
