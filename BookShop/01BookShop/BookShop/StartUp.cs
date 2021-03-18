009c7cb43a15df8b9880fe45c081c017edde7e1f
vebili <74007431+vebili@users.noreply.github.com>
2021-03-18 14:29:52 +0200

    Add project files.

---
+++ BookShop/01BookShop/BookShop/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Text;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            Console.Write("Enter the Age Restriction: ");
+            string commandEnter = Console.ReadLine();
+
+            Console.WriteLine(GetBooksByAgeRestriction(db, commandEnter));
+
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var restrictNum = (AgeRestriction)Enum.Parse(typeof(AgeRestriction), command, true);
+
+            var restrictBooks = context.Books
+                .Where(b => b.AgeRestriction == restrictNum)
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in restrictBooks)
+            {
+                sb.AppendLine($"{book}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
