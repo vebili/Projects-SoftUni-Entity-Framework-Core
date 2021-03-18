52e4b834aad95728829ef1d417097dea41298c93
vebili <74007431+vebili@users.noreply.github.com>
2021-03-18 14:40:53 +0200

    Add project files.

---
+++ BookShop/03BookShop/BookShop/StartUp.cs
@@ -0,0 +1,79 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Text;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            //Console.Write("Enter the Age Restriction: ");
+            //string commandEnter = Console.ReadLine();
+
+            Console.WriteLine(GetBooksByPrice(db));
+
+        }
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var bookPrices = context.Books
+                .Where(b => b.Price > 40)
+                .Select(b => new
+                {
+                    BookTitle = b.Title,
+                    BookPrice = b.Price
+                })
+                .OrderByDescending(sb => sb.BookPrice)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var book in bookPrices)
+            {
+                sb.AppendLine($"{book.BookTitle} - ${book.BookPrice:F2}");
+            }
+            return sb.ToString().Trim();
+        }
+
+        //================================================================================
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var goldEdition = EditionType.Gold;
+
+            var goldBooks = context.Books
+                .Where(gb => gb.EditionType == goldEdition && gb.Copies < 5000)
+                .OrderBy(gb => gb.BookId)
+                .Select(gb => gb.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, goldBooks).Trim();
+        }
+
+        //====================================================================================
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var restrictNum = (AgeRestriction)Enum.Parse(typeof(AgeRestriction), command, true);
+
+            var restrictBooks = context.Books
+                .Where(b => b.AgeRestriction == restrictNum)
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in restrictBooks)
+            {
+                sb.AppendLine($"{book}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ BookShop/04BookShop/BookShop/StartUp.cs
@@ -0,0 +1,93 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Text;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            Console.Write("Enter the Year: ");
+            int commandEnter = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBooksNotReleasedIn(db, commandEnter));
+
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var booksNotReleased = context.Books
+                .Where(b => !(b.ReleaseDate.HasValue && b.ReleaseDate.Value.Year == year))
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, booksNotReleased).Trim();
+        }
+
+        //===========================================================================
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var bookPrices = context.Books
+                .Where(b => b.Price > 40)
+                .Select(b => new
+                {
+                    BookTitle = b.Title,
+                    BookPrice = b.Price
+                })
+                .OrderByDescending(sb => sb.BookPrice)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var book in bookPrices)
+            {
+                sb.AppendLine($"{book.BookTitle} - ${book.BookPrice:F2}");
+            }
+            return sb.ToString().Trim();
+        }
+
+        //================================================================================
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var goldEdition = EditionType.Gold;
+
+            var goldBooks = context.Books
+                .Where(gb => gb.EditionType == goldEdition && gb.Copies < 5000)
+                .OrderBy(gb => gb.BookId)
+                .Select(gb => gb.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, goldBooks).Trim();
+        }
+
+        //====================================================================================
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var restrictNum = (AgeRestriction)Enum.Parse(typeof(AgeRestriction), command, true);
+
+            var restrictBooks = context.Books
+                .Where(b => b.AgeRestriction == restrictNum)
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in restrictBooks)
+            {
+                sb.AppendLine($"{book}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
