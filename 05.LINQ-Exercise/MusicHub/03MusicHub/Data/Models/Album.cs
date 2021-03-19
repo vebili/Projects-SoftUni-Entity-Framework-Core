629ae7334c8596000d43667d63bed64dd97e3571
vebili <74007431+vebili@users.noreply.github.com>
2021-03-19 19:49:57 +0200

    Add project files.

---
+++ 05.LINQ-Exercise/MusicHub/03MusicHub/Data/Models/Album.cs
@@ -0,0 +1,29 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Album
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "DATE")]
+        public DateTime ReleaseDate { get; set; }
+
+        [NotMapped]
+        public decimal Price => this.Songs.Sum(x => x.Price);
+
+        public int? ProducerId { get; set; }
+        public Producer Producer { get; set; }
+
+        public ICollection<Song> Songs { get; set; } = new HashSet<Song>();
+    }
+}
