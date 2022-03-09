a2bb5b40610cc2a7f706c1508b4817a31a38dc7b
vebili <74007431+vebili@users.noreply.github.com>
2022-03-09 11:27:36 +0200

    Add

---
+++ 13.Exam Preparation/Exam 10.12.2017/FastFood/FastFood.Data/Configuration.cs
@@ -0,0 +1,8 @@
+namespace FastFood.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=.;Database=FastFood;Trusted_Connection=True";
+    }
+}
